89907525a7c28d3fc38e9e9ff77d809260e4c132
root <root@ip-172-31-85-235.ec2.internal>
2017-10-31 03:07:31 +0000

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,6 @@ git submodule update --init --recursive
 make install
 export PATH=$PATH:/usr/local/bin
 echo "export PATH=$PATH:/usr/local/bin" >> /root/.bash_profile
-source /root/.bash_profile
 
 # create aws environment
 cd /usr/local/src
@@ -37,7 +36,7 @@ cd $WEB
 # >>> RunWithBash <<<
 REGION="us-east-1"
 DATE=$(date "+%Y-%m-%d-%H-%M-%S")
-cd RunWithBash
+cd RunWithBashUsingExport
 ./createEnvironment.sh $REGION > /usr/local/src/automation-$DATE.log
 if [ $? -ne 0 ];then
 exit 1
@@ -47,11 +46,18 @@ fi
 NEWWEBSERVERIP=$(aws ec2 describe-instances --region="$REGION" --filter Name=tag:Server,Values=Web1  --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
 # Copy Automation-Test.pem and use ssh agent forwarding
-aws s3 cp s3://automation-test-dumlu/Automation-Test.pem /root/.ssh/
+aws s3 cp s3://automation-test-dumlu/Automation-Test.pem /tmp/
 #aws s3 rm s3://automation-test-dumlu/Automation-Test.pem
-chmod 400 /root/.ssh/Automation-Test.pem
+chmod 400 /tmp/Automation-Test.pem
+
+cat >> /root/.bash_profile <<EOL
+if [ -z "\$SSH_AUTH_SOCK" ] ; then
+  eval \$(ssh-agent)
+fi
+EOL
+
 eval $(ssh-agent)
-ssh-add -k /root/.ssh/Automation-Test.pem
+ssh-add -k /tmp/Automation-Test.pem
 
 # Install apps and deploy website via ansible
 cd ansible
@@ -59,6 +65,7 @@ export PATH=$PATH:/usr/local/bin
 export ANSIBLE_HOST_KEY_CHECKING=False
 echo "[webservers]" > ./hosts
 echo $NEWWEBSERVERIP >> ./hosts
+sleep 120 # wait for ssh start
 ansible webservers -a "cat /etc/hostname" -i hosts -u ec2-user
 DATE=$(date "+%Y-%m-%d-%H-%M-%S")
 ansible-playbook web-automation.yml -i hosts -u ec2-user > /usr/local/src/ansible-$DATE.log
