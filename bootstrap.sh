85380bdd28c0a4d9f4191f27f1eb9d310f21da57
root <root@ip-172-31-91-208.ec2.internal>
2017-10-31 03:49:47 +0000

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,21 +27,28 @@ echo "export PATH=$PATH:/usr/local/bin" >> /root/.bash_profile
 # create aws environment
 cd /usr/local/src
 git clone https://github.com/oceanosis/website-automation
-WEB=/usr/local/src/website-automation
-cd $WEB
 
 ###################################################################
 # Type of automation: Bash, Ansible, Pyhton : change medhodology  #
 ###################################################################
-# >>> RunWithBash <<<
+cat >> /root/.bash_profile <<EOL
+if [ -z "\$SSH_AUTH_SOCK" ] ; then
+  eval \$(ssh-agent)
+fi
+EOL
+
+eval $(ssh-agent)
+DIR=/usr/local/src/website-automation/RunWithBashUsingExport
+
 REGION="us-east-1"
 DATE=$(date "+%Y-%m-%d-%H-%M-%S")
-cd RunWithBashUsingExport
-./createEnvironment.sh $REGION > /usr/local/src/automation-$DATE.log
+
+$DIR/createEnvironment.sh $REGION > /usr/local/src/automation-$DATE.log
 if [ $? -ne 0 ];then
 exit 1
 fi
-#Environment Creation is over, now deployment
+
+# Environment Creation is over, now deployment
 # For only one server... ( will be changed )
 NEWWEBSERVERIP=$(aws ec2 describe-instances --region="$REGION" --filter Name=tag:Server,Values=Web1  --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
@@ -50,23 +57,21 @@ aws s3 cp s3://automation-test-dumlu/Automation-Test.pem /tmp/
 #aws s3 rm s3://automation-test-dumlu/Automation-Test.pem
 chmod 400 /tmp/Automation-Test.pem
 
-cat >> /root/.bash_profile <<EOL
-if [ -z "\$SSH_AUTH_SOCK" ] ; then
-  eval \$(ssh-agent)
-fi
-EOL
-
-eval $(ssh-agent)
 ssh-add -k /tmp/Automation-Test.pem
 
 # Install apps and deploy website via ansible
-cd ansible
+cd $DIR/ansible
 export PATH=$PATH:/usr/local/bin
 export ANSIBLE_HOST_KEY_CHECKING=False
 echo "[webservers]" > ./hosts
 echo $NEWWEBSERVERIP >> ./hosts
-sleep 120 # wait for ssh start
+sleep 60 # wait for ssh start
 ansible webservers -a "cat /etc/hostname" -i hosts -u ec2-user
 DATE=$(date "+%Y-%m-%d-%H-%M-%S")
 ansible-playbook web-automation.yml -i hosts -u ec2-user > /usr/local/src/ansible-$DATE.log
+if [ $? -ne 0 ]; then
+ansible-playbook web-automation.yml -i hosts -u ec2-user > /usr/local/src/ansible-$DATE.log
+else
+echo "Ansible playbook did not completed. Check process and errors in log"
+fi
 # 
