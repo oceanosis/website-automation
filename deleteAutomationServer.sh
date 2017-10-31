9a9ea0e31c4d37af89b52000d54e1f469334466c
root <root@ip-172-31-91-208.ec2.internal>
2017-10-31 03:50:57 +0000

    deleteAutoServer

--- deleteAutomationServer.sh
+++ deleteAutomationServer.sh
@@ -4,4 +4,4 @@
 
 InstanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-aws ec2 terminate-instances --instance-ids="$InstanceID"
+aws ec2 terminate-instances --instance-ids="$InstanceID" --region=$1
